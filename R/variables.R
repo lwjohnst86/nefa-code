c80cbda0ddd3ff101cbf7e0fc2123c3492fcd4e6
Luke W Johnston <lwjohnst@gmail.com>
2017-06-29 23:37:32 -0400

    added covar to variables

--- R/variables.R
+++ R/variables.R
@@ -1,3 +1,4 @@
 outcomes <- c('linvHOMA', 'lISI', 'lIGIIR', 'lISSI2')
 ne_pct <- grep('^pct_ne', vars, value = TRUE)
 ne_conc <- grep('^ne\\d\\d', vars, value = TRUE)
+covariates <- c('VN', 'Waist', 'Sex', 'Ethnicity', 'BaseAge', 'ALT', 'FamHistDiab')
