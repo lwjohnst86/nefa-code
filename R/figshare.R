29be9be06ae919a470b230ed44194ad169b1c9ea
Luke W Johnston <lwjohnst@gmail.com>
2017-06-30 11:26:48 -0400

    reformat figshare fun

--- R/figshare.R
+++ R/figshare.R
@@ -7,19 +7,27 @@ library(rfigshare)
 # 4. git archive --format=zip --output=git-nefa-code.zip manuscript-code #(branch name)
 # 5. then run the below code
 send_to_figshare <- function() {
-fs_new_article("Analysis code for study on non-esterified fatty acids and the pathogenesis of diabetes",
-                     "R code used for the analysis in the NEFA manuscript.",
-                     type = "fileset",
-                     # there was a problem with the authors...
-                     # authors = c("Luke Johnston", "Stewart Harris", "Ravi Retnakaran",
-                     #             "Adria Giacca", "Zhen Liu", "Richard P. Bazinet",
-                     #             "Anthony J. Hanley"),
-                     tags = c("insulin sensitivity", "beta-cell function", "fatty acids",
-                              "non-esterified fatty acids", "free fatty acids",
-                              "cohort", "longitudinal"),
-                     categories = c("Diseases", "Pathogenesis", "Epidemiology"),
-                     #links = "link to github",
-                    links = "https://github.com/lwjohnst86/nefa-code",
-                     files = "code-archive.zip",
-                     visibility = "draft")
+    fs_new_article(
+        "Analysis code for study on non-esterified fatty acids and the pathogenesis of diabetes",
+        "R code used for the analysis in the NEFA manuscript.",
+        type = "fileset",
+        # there was a problem with the authors...
+        # authors = c("Luke Johnston", "Stewart Harris", "Ravi Retnakaran",
+        #             "Adria Giacca", "Zhen Liu", "Richard P. Bazinet",
+        #             "Anthony J. Hanley"),
+        tags = c(
+            "insulin sensitivity",
+            "beta-cell function",
+            "fatty acids",
+            "non-esterified fatty acids",
+            "free fatty acids",
+            "cohort",
+            "longitudinal"
+        ),
+        categories = c("Diseases", "Pathogenesis", "Epidemiology"),
+        #links = "link to github",
+        links = "https://github.com/lwjohnst86/nefa-code",
+        files = "code-archive.zip",
+        visibility = "draft"
+    )
 }
