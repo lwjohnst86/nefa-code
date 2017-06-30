97357ef8061825d0fcb8be7d6902b50fde0150c3
Luke W Johnston <lwjohnst@gmail.com>
2017-06-30 00:28:38 -0400

    updated vars VN to yearsfrombaseline

--- R/variables.R
+++ R/variables.R
@@ -1,4 +1,5 @@
 outcomes <- c('linvHOMA', 'lISI', 'lIGIIR', 'lISSI2')
 ne_pct <- grep('^pct_ne', vars, value = TRUE)
 ne_conc <- grep('^ne\\d\\d', vars, value = TRUE)
-covariates <- c('VN', 'Waist', 'Sex', 'Ethnicity', 'BaseAge', 'ALT', 'FamHistDiab')
+ne_total <- "TotalNE"
+covariates <- c("YearsFromBaseline", 'Waist', 'Sex', 'BiEthnicity', 'BaseAge', 'ALT', 'FamHistDiab')
