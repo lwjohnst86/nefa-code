ca706903af2c342d5b4678b518060459ad3480a9
Luke W Johnston <lwjohnst@gmail.com>
2017-06-30 23:09:53 -0400

    homa-ir -> homa2

--- R/variables.R
+++ R/variables.R
@@ -1,4 +1,4 @@
-outcomes <- c('linvHOMA', 'lISI', 'lIGIIR', 'lISSI2')
+outcomes <- c('lHOMA2_S', 'lISI', 'lIGIIR', 'lISSI2')
 ne_pct <- grep('^pct_ne', vars, value = TRUE)
 ne_conc <- grep('^ne\\d\\d', vars, value = TRUE)
 ne_total <- "TotalNE"
