14b7221c392fc079695f3cdd5da2f7ecb9fc24e9
Luke W Johnston <lwjohnst@gmail.com>
2017-06-30 22:15:00 -0400

    updated/moved fun to calc incident dysgly

--- R/data_prep.R
+++ R/data_prep.R
@@ -17,3 +17,38 @@ order_by_fattyacid <- function(data, fa_col) {
         dplyr::select(-order1) %>%
         dplyr::rename_(.dots = stats::setNames(names(.), gsub("Measure", fa_col, names(.))))
 }
+
+#' Calculate incident dysglycemia over the 6 years.
+#'
+#' @param data project data
+#'
+incident_dysglycemia_data <- function(data) {
+    dysgly_data <-
+        dplyr::left_join(
+            data %>%
+                dplyr::filter(VN == 0),
+            data %>%
+                dplyr::filter(!is.na(TotalNE)) %>%
+                dplyr::mutate_at(c("IFG", "IGT"), dplyr::funs(ifelse(is.na(.), 0, .))) %>%
+                dplyr::mutate(PreDM = as.numeric(rowSums(.[c('IFG', 'IGT')], na.rm = TRUE))) %>%
+                dplyr::mutate(FactorDysgly = ifelse(
+                    PreDM == 1, 'PreDM',
+                    ifelse(DM == 1, 'DM',
+                           'NGT')
+                )) %>%
+                dplyr::select(SID, VN, FactorDysgly) %>%
+                tidyr::spread(VN, FactorDysgly) %>%
+                dplyr::mutate(
+                    DetailedConvert = as.factor(paste0(`0`, '-', `1`, '-', `2`)),
+                    ConvertDysgly = as.numeric(!grepl('NGT$|NGT-NA$|NGT-NGT-NA$|NGT-NA-NA$',
+                                                      DetailedConvert)),
+                    ConvertDM = as.numeric(grepl('-DM$|-DM-DM$|-DM-NA$|-DM-NGT$',
+                                                 DetailedConvert)),
+                    ConvertPreDM = as.numeric(grepl('-PreDM$|-PreDM-PreDM$|-PreDM-NA$',
+                                                    DetailedConvert))
+                )
+        ) %>%
+        dplyr::filter(!is.na(TotalNE))
+
+    return(dysgly_data)
+}
