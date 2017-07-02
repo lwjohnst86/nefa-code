39faece8cc4666dec9c6c392f421414db1b230db
Luke W Johnston <lwjohnst@gmail.com>
2017-07-02 14:18:36 -0400

    added fun to calc followup time

--- R/calculate.R
+++ R/calculate.R
@@ -120,3 +120,16 @@ extract_gee_estimateCI <- function(data) {
       dplyr::arrange(dep, dplyr::desc(estimate)) %>%
       dplyr::select(dep, indep, estCI)
 }
+
+#' Mean and SD followup time for those who attended the 6 year visit.
+#'
+#' @param data Project data
+#'
+calc_followup_time <- function(data = project_data) {
+    data %>%
+        dplyr::arrange(SID, VN) %>%
+        dplyr::group_by(SID) %>%
+        dplyr::slice(n()) %>%
+        dplyr::ungroup() %>%
+        dplyr::summarise(MeanFollowup = aide::ave_sd(YearsFromBaseline)) %>%
+        .[[1]]
