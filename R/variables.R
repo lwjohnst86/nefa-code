b79cad9d6cbb4250bcc33acee4bc4678681bc0b4
Luke W Johnston <lwjohnst@gmail.com>
2017-07-03 19:22:18 -0400

    Updated model variables based on qic

--- R/variables.R
+++ R/variables.R
@@ -2,4 +2,4 @@ outcomes <- c('lHOMA2_S', 'lISI', 'lIGIIR', 'lISSI2')
 ne_pct <- grep('^pct_ne', vars, value = TRUE)
 ne_conc <- grep('^ne\\d\\d', vars, value = TRUE)
 ne_total <- "TotalNE"
-covariates <- c("YearsFromBaseline", 'Waist', 'Sex', 'BiEthnicity', 'BaseAge', 'ALT', 'FamHistDiab')
+covariates <- c("YearsFromBaseline", 'Waist', 'Sex', 'BiEthnicity', 'BaseAge', 'ALT', 'MET')
