09cce996c804ce19bdafd3eb65dbff9a323dd36e
Luke W Johnston <lwjohnst@gmail.com>
2017-08-08 19:23:26 -0400

    updated manuscript.R from rmd

--- doc/manuscript.R
+++ doc/manuscript.R
@@ -1,14 +1,8 @@
-## ----knit_setup, echo=FALSE----------------------------------------------
-#knitr::opts_knit$set(root.dir = '../')
-
-## ----setup, message=FALSE, echo=FALSE------------------------------------
-#source('.Rprofile')
-devtools::load_all()
+## ----setup, include=FALSE------------------------------------------------
+devtools::load_all('.')
 set_options()
-ds <- project_data
-ds0yr <- dplyr::filter(ds, VN == 0)
 
-## ----caption_setup-------------------------------------------------------
+## ----captions------------------------------------------------------------
 ## Include captions below using `captioner` package
 library(captioner)
 fig <- captioner(prefix = 'Fig.')
@@ -19,190 +13,48 @@ tab <- captioner(prefix = 'Table')
 cite_t <- pryr::partial(tab, display = 'cite')
 stab <- captioner(prefix = 'ESM Table')
 cite_st <- pryr::partial(stab, display = 'cite')
-# usage: cite_st('basicChar')
 
-tab_basic <- tab('basicChar', 'Basic characteristics of the PROMISE participants at each of the 3 clinic visits.')
-fig_dist <- fig('nefaDist', 'Concentrations (nmol/mL) of each NEFA in PROMISE participants at the baseline visit (2004-2006).')
-fig_gee <- fig('gee', 'Longitudinal associations of individual NEFA (mol% and nmol/mL) with insulin sensitivity and beta-cell function over the 6 years in the PROMISE cohort. Fully adjusted generalized estimating equation models. X-axis values represent a percent difference in the outcome per SD increase in the fatty acid. P-values were adjusted for the BH false discovery rate, with the largest dot representing a significant (p<0.05) association.')
+tab_basic <- tab('basic', 'Basic characteristics of the PROMISE participants at each of the three clinic visits.')
+fig_dist <- fig('nefa', 'Concentrations (nmol/mL) of each NEFA in PROMISE participants at the baseline visit (2004-2006).')
+fig_gee_unadj <- fig('gee_unadj', 'Longitudinal associations of individual non-esterified fatty acids (mol% and nmol/mL) with insulin sensitivity and beta-cell function over 6 years in the PROMISE cohort. Models are only adjusted for time. X-axis values represent a percent difference in the outcome per SD increase in the fatty acid. P-values were adjusted for the BH false discovery rate, with each increase in dot size and blackness representing a p-value significance of p>0.05, p<0.05, p<0.01, and p<0.001.')
+fig_gee_adj <- fig('gee_adj', 'Longitudinal associations of individual non-esterified fatty acids (mol% and nmol/mL) with insulin sensitivity and beta-cell function over the 6 years in the PROMISE cohort. Generalized estimating equation models were fully adjusted. X-axis values represent a percent difference in the outcome per SD increase in the fatty acid. P-values were adjusted for the BH false discovery rate, with each increase in dot size and blackness representing a p-value significance of p>0.05, p<0.05, and p<0.01.')
 
-sfig_consort <- sfig('consort', 'CONSORT diagram of sample size at each examination visit.')
-stab_qic <- stab('qic', 'Comparing generalized estimating equation models adjusting for different covariates using Quasi-Likelihood Information Criterion.')
-stab_dist <- stab('nefaDist', 'Raw concentration values (nmol/mL) of each non-esterified fatty acid in the PROMISE cohort at the baseline visit (2004-2006). Values are presented as mean (SD).')
-sfig_corr <- sfig('corr', 'Pearson correlation heatmap of non-esterified fatty acids (nmol/mL) and basic PROMISE participant characteristics for the baseline visit (2004-2006). Darkness of the colour indicates the magnitude of the correlation, with orange indicating positive and blue indicating negative correlations.')
-sfig_gee <- sfig('gee', 'Unadjusted generalized estimating equation modeling of the longitudinal association of individual non-esterified fatty acids (mol% and nmol/mL) with insulin sensitivity and beta-cell function over 6 years in the PROMISE cohort. GEE models are only adjusted for time. Outcome variables were log-transformed, predictor variables were scaled, and x-axis values were exponentiated to represent percent difference per SD increase in the fatty acid. P-values were adjusted for the BH false discovery rate, with the largest dot representing a significant (p<0.05) association (*).')
-stab_gee <- stab('gee', 'Longitudinal associations of individual non-esterified fatty acids (mol% and nmol/mL) with insulin sensitivity and beta-cell function over the 6 years in the PROMISE cohort. GEE models were adjusted for time, sex, ethnicity, baseline age, WC,
-ALT, and family history of diabetes. Outcome variables were log-transformed, predictor variables were scaled, and x-axis values were exponentiated to represent percent difference per SD increase in the fatty acid.  P-values were adjusted for the BH false discovery rate, with significant (p<0.05) associations indicated by asterisk (*).')
-stab_gee_unadj <- stab('gee_unadj', 'Unadjusted generalized estimating equation models of the longitudinal associations of individual non-esterified fatty acids (mol% and nmol/mL) with insulin sensitivity and beta-cell function over the 6 years in the PROMISE cohort.  GEE models are only adjusted for time. Outcome variables were log-transformed, predictor variables were scaled, and x-axis values were exponentiated to represent percent difference per SD increase in the fatty acid.  P-values were adjusted for the BH false discovery rate, with significant (p<0.05) associations indicated by asterisk.')
-sfig_lcmm <- sfig('lcmm', 'Latent class mixed model (LCMM) analysis to identify individual classes of trajectories for log(ISSI-2) over the 6 years in the PROMISE cohort. LCMM is a technique that identifies groups of participants that share a similar underlying trajectory in beta-cell function over the 6 years (e.g. no change compared to declines in ISSI-2). Red lines indicate individuals with a high beta-cell function who stayed high, green represents those in the middle, and blue represents those who had the lowest beta-cell function.')
-sfig_plsda_groups <- sfig('plsda_groups', '')
-sfig_plsda_loadings <- sfig('plsda_loadings', 'Pattern loadings from partial least squares discriminant analysis (PLS-DA) to identify potential clusters of NEFA composition within the classes extracted from the latent class mixed model in 463 participants from the baseline PROMISE visit (2004-2006). The percent explained variance of each component is shown in brackets on each axis. The solid line represents an explained variance of 100% while the dashed line represents an explained variance of 50%. See the ESM Methods for a description of PLS-DA and an explanation of interpreting this plot.')
-sfig_plsda_groups_dysgly <- sfig('plsda_groups_dysgly', '')
-sfig_plsda_loadings_dysgly <- sfig('plsda_loadings_dysgly', 'Pattern loadings from partial least squares discriminant analysis (PLS-DA) to identify potential clusters of NEFA composition for dysglycemia (IFG, IGT, DM) conversion status over the 6-years in the participants from the baseline PROMISE visit (2004-2006). See the ESM Methods for a description of PLS-DA and an explanation of interpreting this plot. The percent explained variance of each component is shown in brackets on each axis. The solid line represents an explained variance of 100% while the dashed line represents an explained variance of 50%.')
-sfig_plsda_example <- sfig('plsda_example', 'Example results of a high discriminatory ability to classify groups accurately when using partial least squares discriminatory analysis (PLS-DA). Discriminatory ability is evident from the amount of separation between the two groups. In this case, PLS-DA was 82% accurate at correctly classifying groups. See the ESM Methods for a description of PLS-DA and for interpreting this plot.')
-sfig_plsda_groups <- sfig('plsda_groups', paste0('Clustering of extracted components from the partial least squares discriminant analysis (PLS-DA) on the classes extracted from the latent class mixed model (LCMM) in 463 participants from the baseline PROMISE visit (2004-2006). See the ESM Methods for a description of PLS-DA and interpreting this plot. The percent explained variance of each component is shown in brackets on each axis. Red, green, and blue lines indicate participants classified as high, middle, and low for beta-cell function, respectively, from the LCMM analysis. See ', cite_sf('plsda_example'), ' for a hypothetical example plot showing good discriminatory ability between groups.'))
-sfig_plsda_groups_dysgly <- sfig('plsda_groups_dysgly', paste0('Clustering of extracted components from the partial least squares discriminant analysis for dysglycemia (IFG, IGT, DM) conversion status over the 6-years in the participants from the baseline PROMISE visit (2004-2006).  See the ESM Methods for a description of PLS-DA and interpreting this plot. The percent explained variance of each component is shown in brackets on each axis. Blue lines indicate dysglycemia conversion or maintanence and red lines indicate no dysglycemia status. See ', cite_sf('plsda_example'), ' for a hypothetical example plot showing good discriminatory ability between groups.'))
+## ----inline_results------------------------------------------------------
+nefa <- calc_pct_nefa(project_data)
+chg_outcomes <- calc_outcomes_chg_n(project_data)
 
-## ----variableAndDataSet, message=FALSE, results='hide'-------------------
-gee_df <- get_gee_data(ds)
-dysgly_data <- get_dysglycemia_data(ds)
+ds0yr <- dplyr::filter(project_data, VN == 0)
+female <- calc_npercent(ds0yr$Sex, 'Female')
+european <- calc_npercent(ds0yr$Ethnicity, 'European')
+famhist <- calc_npercent(ds0yr$FamHistDiab, 'Yes')
+est_gee <- gee_results %>% 
+    dplyr::filter(model == "Adjusted") %>% 
+    calc_gee_magnitude()
 
-outcomes_is <- c('linvHOMA', 'lISI')
-outcomes_bcf <- c('lIGIIR', 'lISSI2')
-outcomes <- c(outcomes_is, outcomes_bcf)
-ne_pct <- grep('^pct_ne', names(gee_df), value = TRUE)
-ne_conc <- c('TotalNE', grep('^ne\\d\\d', names(gee_df), value = TRUE))
-covariates <- c('VN', 'Waist', 'Sex', 'Ethnicity', 'BaseAge', 'ALT', 'FamHistDiab')
+meds_use <- table(project_data$MedsLipidsChol, project_data$VN)[2, c(1, 3)]
 
-## ----geeData, cache=TRUE-------------------------------------------------
-gee_results <- dplyr::bind_rows(
-    analyze_gee(gee_df, outcomes_is, ne_pct, covariates, 'mol%', adj.p = TRUE),
-    analyze_gee(gee_df, outcomes_is, ne_conc, covariates, 'nmol/mL', adj.p = TRUE),
-    analyze_gee(gee_df, outcomes_bcf, ne_pct, covariates, 'mol%', adj.p = TRUE),
-    analyze_gee(gee_df, outcomes_bcf, ne_conc, covariates, 'nmol/mL', adj.p = TRUE)
-    ) %>% 
-    tidy_gee_results()
+sig_fa_unadj <- gee_results %>% 
+    dplyr::filter(model == "Unadjusted", p.value < 0.05) %>% 
+    dplyr::select(Yterms, Xterms, unit) %>% 
+    dplyr::mutate(Xterms = ifelse(Xterms == "Total", "total NEFA", as.character(Xterms)))
+sig_fa_adj <- gee_results %>% 
+    dplyr::filter(model == "Adjusted", p.value < 0.05) %>% 
+    dplyr::select(Yterms, Xterms, unit) %>% 
+    dplyr::mutate(Xterms = ifelse(Xterms == "Total", "total NEFA", as.character(Xterms)))
 
-gee_results_unadj <- dplyr::bind_rows(
-    analyze_gee(gee_df, outcomes_is, ne_pct, 'VN', 'mol%', adj.p = TRUE),
-    analyze_gee(gee_df, outcomes_is, ne_conc, 'VN', 'nmol/mL', adj.p = TRUE),
-    analyze_gee(gee_df, outcomes_bcf, ne_pct, 'VN', 'mol%', adj.p = TRUE),
-    analyze_gee(gee_df, outcomes_bcf, ne_conc, 'VN', 'nmol/mL', adj.p = TRUE)
-    ) %>% 
-    tidy_gee_results()
+## ----table1_basic--------------------------------------------------------
+table_basic(project_data, caption = tab_basic)
 
-## ----lcmmPls, cache=TRUE, results='hide'---------------------------------
-# include also the 3-yr and 6-yr data for training/testing.
-lcmm_results <- analyze_lcmm(ds)
-lcmm_data <- combine_lcmm_data(ds, lcmm_results)
-plsda_results <- analyze_plsda(lcmm_data)
-plsda_dysgly <- dysgly_data %>% 
-    dplyr::mutate(ConvertDysgly = factor(ConvertDysgly, levels = c(0, 1), labels = c('No', 'Yes'))) %>% 
-    analyze_plsda('ConvertDysgly')
+## ----figure1_nefa, fig.cap=fig_dist--------------------------------------
+plot_nefa_distribution()
 
-## ----inlineResults-------------------------------------------------------
-percent.nefa <- calculate_percent_nefa_contribution(ds) 
-misclass.n.percent.bcf <- calculate_plsda_misclass(plsda_results)
-misclass.n.percent.dysgly <- calculate_plsda_misclass(plsda_dysgly)
-percent.change.outcomes <- calculate_outcomes_pct_change(ds)
-lcmm.groups <- calculate_ngroups_lcmm(lcmm_results)
-dm.convert <- calculate_conversion_dysgly(dysgly_data, 'ConvertDM')
-predm.convert <- calculate_conversion_dysgly(dysgly_data, 'ConvertPreDM')
-sex.npct <- calculate_factor_npercent(ds0yr$Sex, 'Female')
-ethn.npct <- calculate_factor_npercent(ds0yr$Ethnicity, 'European')
-fhd.npct <- calculate_factor_npercent(ds0yr$FamHistDiab, 'Yes')
-est.gee <- calculate_gee_magnitude(gee_results)
-sample.outcomes <- calculate_sample(ds)
+## ----figure2_gee_unadj, fig.cap=fig_gee_unadj----------------------------
+gee_results %>% 
+    dplyr::filter(model == "Unadjusted") %>% 
+    plot_gee_main()
 
-## ----table1--------------------------------------------------------------
-table_basic(ds, caption = tab_basic)
-
-## ----figure1, fig.cap=fig_dist-------------------------------------------
-plot_nefa_distribution(ds)
-
-## ----figure2, dependson='geeData', fig.cap=fig_gee-----------------------
-plot_gee_results(gee_results, leg = 'none')
-
-## ----ESM_Table1----------------------------------------------------------
-library(magrittr)
-prep_qic <- gee_df %>%
-    dplyr::select(
-        SID, VN, lISSI2, lISI, TotalNE, Sex, Ethnicity, MET, ALT, BaseAge,
-        Waist, BMI, AlcoholPerWk, TobaccoUse, FamHistDiab
-        ) 
-
-qic_is <- prep_qic %>% 
-    dplyr::select(-lISSI2) %>% 
-    stats::na.omit()
-
-qic_bcf <- prep_qic %>% 
-    dplyr::select(-lISI) %>% 
-    stats::na.omit()
-
-# QIC for IS
-M0 <- geepack::geeglm(
-    lISI ~ TotalNE + VN, data = qic_is,
-    id = SID, family = stats::gaussian,
-    corstr = 'ar1'
-    )
-
-
-M1 <- stats::update(M0, . ~ . + Waist + BaseAge + Ethnicity + Sex)
-M2 <- stats::update(M0, . ~ . + Waist + BaseAge + Ethnicity + Sex + ALT) # best
-M3 <- stats::update(M0, . ~ . + Waist + BaseAge + Ethnicity + Sex + ALT + MET) # best
-M4 <- stats::update(M0, . ~ . + Waist + BaseAge + Ethnicity + Sex + ALT + MET + AlcoholPerWk) # best
-M5 <- stats::update(M0, . ~ . + Waist + BaseAge + Ethnicity + Sex + ALT + MET + AlcoholPerWk + FamHistDiab) # best
-M6 <- stats::update(M0, . ~ . + Waist + BaseAge + Ethnicity + Sex + ALT + MET + AlcoholPerWk + FamHistDiab + TobaccoUse) # best
-M7 <- stats::update(M0, . ~ . + Waist + BaseAge + Ethnicity + Sex + ALT + FamHistDiab + TobaccoUse) # best
-M8 <- stats::update(M0, . ~ . + Waist + BaseAge + Ethnicity + Sex + ALT + FamHistDiab) # best
-M9 <- stats::update(M0, . ~ . + Waist + BaseAge + Ethnicity + Sex + ALT + FamHistDiab + TotalNE:VN)
-
-qic_is_tab <- MuMIn::model.sel(M0, M1, M2, M3, M4, M5, M6, M7, M8, M9, rank = MuMIn::QIC) %>% 
-    tibble::rownames_to_column() %>%
-    dplyr::select(Model = rowname, QIC = IC, Delta = delta) %>%
-    dplyr::mutate(id = seq_len(nrow(.))) %>%
-    dplyr::bind_rows(., data.frame(Model = '**log(ISI)**', id = 0.5))
-    
-# QIC data for BCF
-qic_bcf <- prep_qic %>% 
-    dplyr::select(-lISI) %>% 
-    stats::na.omit()
-
-M0 <- geepack::geeglm(
-    lISSI2 ~ TotalNE + VN, data = qic_bcf,
-    id = SID, family = stats::gaussian,
-    corstr = 'ar1'
-    )
-M1 <- stats::update(M0, . ~ . + Waist + BaseAge + Ethnicity + Sex)
-M2 <- stats::update(M0, . ~ . + Waist + BaseAge + Ethnicity + Sex + ALT) 
-M3 <- stats::update(M0, . ~ . + Waist + BaseAge + Ethnicity + Sex + ALT + MET)
-M4 <- stats::update(M0, . ~ . + Waist + BaseAge + Ethnicity + Sex + ALT + MET + AlcoholPerWk)
-M5 <- stats::update(M0, . ~ . + Waist + BaseAge + Ethnicity + Sex + ALT + MET + AlcoholPerWk + FamHistDiab) # best
-M6 <- stats::update(M0, . ~ . + Waist + BaseAge + Ethnicity + Sex + ALT + MET + AlcoholPerWk + FamHistDiab + TobaccoUse) # best
-M7 <- stats::update(M0, . ~ . + Waist + BaseAge + Ethnicity + Sex + ALT + FamHistDiab + TobaccoUse) # best
-M8 <- stats::update(M0, . ~ . + Waist + BaseAge + Ethnicity + Sex + ALT + FamHistDiab) # best
-M9 <- stats::update(M0, . ~ . + Waist + BaseAge + Ethnicity + Sex + ALT + FamHistDiab + TotalNE:VN) # best
-
-qic_bcf_tab <- MuMIn::model.sel(M0, M1, M2, M3, M4, M5, M6, M7, M8, M9, rank = MuMIn::QIC) %>% 
-    tibble::rownames_to_column() %>%
-    dplyr::select(Model = rowname, QIC = IC, Delta = delta) %>% 
-    dplyr::mutate(id = seq_len(nrow(.))+10) %>%
-    dplyr::bind_rows(., data.frame(Model = '**log(ISSI-2)**', id = 10.5))
-
-rbind(qic_is_tab, qic_bcf_tab) %>% 
-    dplyr::arrange(id) %>%
-    dplyr::select(-id) %>%
-    dplyr::mutate(QIC = round(QIC, 1),
-           Delta = round(Delta, 1)) %>% 
-    pander::pander(missing = '', caption = stab_qic)
-
-## ----ESM_Table2----------------------------------------------------------
-table_distribution(ds, stab_dist)
-
-## ----ESM_Fig2, fig.cap=sfig_corr-----------------------------------------
-plot_heatmap(ds)
-
-## ----ESM_Fig3, fig.cap=sfig_gee------------------------------------------
-plot_gee_results(gee_results_unadj)
-
-## ----ESM_Table3_4--------------------------------------------------------
-table_gee(gee_results, stab_gee)
-table_gee(gee_results_unadj, stab_gee_unadj)
-
-## ----ESM_Fig4, fig.cap=sfig_lcmm-----------------------------------------
-plot_lcmm_results(lcmm_data)
-
-## ----ESM_Fig5, fig.cap=sfig_plsda_groups---------------------------------
-plot_plsda_grouping(plsda_results, 'Beta-cell function')
-
-## ----ESM_Fig6, fig.cap=sfig_plsda_loadings, fig.width=7, fig.height=7----
-plot_plsda_loadings(plsda_results)
-
-## ----ESM_Fig7, fig.cap=sfig_plsda_groups_dysgly--------------------------
-plot_plsda_grouping(plsda_dysgly, 'Dysglycemia')
-
-## ----ESM_Fig8, fig.cap=sfig_plsda_loadings_dysgly, fig.width=7, fig.height=7----
-plot_plsda_loadings(plsda_dysgly)
-
-## ----ESM_Fig9, fig.cap=sfig_plsda_example--------------------------------
-plot_plsda_grouping(example_plsda_results(), 'Example groups')
+## ----figure3_gee_adj, fig.cap=fig_gee_adj--------------------------------
+gee_results %>% 
+    dplyr::filter(model == "Adjusted") %>% 
+    plot_gee_main()
 
